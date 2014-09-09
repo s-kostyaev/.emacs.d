1aa271dcecdc410f89552698ae05a1f345e60f68
Kostyaev Sergey <s.kostyaev@office.ngs.ru>
2014-09-09 19:59:40 +0000

    Add some stuff to emacs

---
+++ elpa/color-theme-library-0.0.10/color-theme-library-autoloads.el
@@ -0,0 +1,18 @@
+;;; color-theme-library-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("color-theme-library-pkg.el" "color-theme-library.el")
+;;;;;;  (21517 36626 460676 139000))
+
+;;;***
+
+(provide 'color-theme-library-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; color-theme-library-autoloads.el ends here
