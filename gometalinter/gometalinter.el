c28afdf26f793efcfb8733c8dee00cbd2e380ee1
Sergey Kostyaev <s.kostyaev@office.ngs.ru>
2015-11-18 16:20:16 +0600

    add gometalinter

---
+++ gometalinter/gometalinter.el
@@ -0,0 +1,29 @@
+;; Modified golint.el to provide gometalinter
+
+(require 'compile)
+
+(defun go-lint-buffer-name (mode)
+  "*Gometalinter*")
+
+(defun gometalinter-process-setup ()
+  "Setup compilation variables and buffer for `gometalinter'."
+  (run-hooks 'gometalinter-setup-hook))
+
+(define-compilation-mode gometalinter-mode "gometalinter"
+  "Gometalinter is a linter for Go source code."
+  (set (make-local-variable 'compilation-scroll-output) nil)
+  (set (make-local-variable 'compilation-disable-input) t)
+  (set (make-local-variable 'compilation-process-setup-function)
+       'gometalinter-process-setup)
+  )
+
+;;;###autoload
+(defun gometalinter ()
+  "Run gometalinter on the current file and populate the fix list. Pressing C-x ` will jump directly to the line in your code which caused the first message."
+  (interactive)
+  (compilation-start
+   (mapconcat #'shell-quote-argument
+	      (list "gometalinter") " ")
+   'gometalinter-mode))
+
+(provide 'gometalinter)
