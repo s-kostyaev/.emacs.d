2f6f2398c0c63fc3685331ae6566c8e510cb4c2a
Sergey Kostyaev <s.kostyaev@office.ngs.ru>
2016-05-06 00:00:41 +0600

    fix emacs packages auto install

--- init.el
+++ init.el
@@ -25,6 +25,10 @@
 ;; (when (< emacs-major-version 24)
 ;;     ;; For important compatibility libraries like cl-lib
 ;;     (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(setq  package-archives
+   (quote
+    (("gnu" . "http://elpa.gnu.org/packages/")
+     ("melpa" . "http://melpa.org/packages/"))))
 (package-initialize) ;; You might already have this line
 
 										; for automatic download required packages
