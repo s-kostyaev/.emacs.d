047cc0df14c1eb66a22cbaf75cb6d9fbe880b822
Sergey Kostyaev <sergey.kostyaev@eltex.loc>
2016-05-06 11:56:07 +0600

    remap key for scroll-down-command

--- init.el
+++ init.el
@@ -709,6 +709,7 @@ the end of the line, then comment current line.  Replaces default behaviour of
 (global-set-key (kbd "s-x") 'counsel-M-x)
 (global-set-key (kbd "s-y") 'counsel-yank-pop)
 (global-set-key (kbd "s-w") 'kill-ring-save)
+(global-set-key (kbd "s-v") 'scroll-down-command)
 (global-set-key (kbd "C-c C-s") 'counsel-ag)
 (global-set-key (kbd "C-x C-f") 'counsel-find-file)
 (need-package 'counsel-projectile)
