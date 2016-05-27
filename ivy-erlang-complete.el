cb10b7b405e2608dd0e2d4403067ba2aee1c273a
Sergey Kostyaev <s.kostyaev@office.ngs.ru>
2016-05-27 22:16:04 +0600

    rename counsel-erl to ivy-erlang-complete

--- ivy-erlang-complete.el
+++ ivy-erlang-complete.el
@@ -1,4 +1,4 @@
-;;; counsel-erl --- Erlang completion at point using ivy.
+;;; ivy-erlang-complete --- Erlang completion at point using ivy.
 
 ;;; Commentary:
 ;; Deps:
@@ -9,50 +9,50 @@
 
 
 ;;; Code:
-(require 'counsel)
+(require 'ivy)
 (require 'subr-x)
 (require 'dash)
 
-(defvar counsel-erl-erlang-root "/usr/lib/erlang"
+(defvar ivy-erlang-complete-erlang-root "/usr/lib/erlang"
   "Path to erlang root.")
 
-(defvar counsel-erl-project-root)
+(defvar ivy-erlang-complete-project-root)
 
-(defvar-local counsel-erl-candidates nil
+(defvar-local ivy-erlang-complete-candidates nil
   "Candidates for completion.")
 
-(defvar-local counsel-erl-predicate nil
+(defvar-local ivy-erlang-complete-predicate nil
   "Completion predicate.")
 
-(defun counsel-erl--find-functions (module)
+(defun ivy-erlang-complete--find-functions (module)
   "Find functions in MODULE."
   (-remove #'string-empty-p
    (split-string
     (shell-command-to-string
      (concat
       "find "
-      counsel-erl-project-root
+      ivy-erlang-complete-project-root
       " "
-      counsel-erl-erlang-root
+      ivy-erlang-complete-erlang-root
       " -name "
       module
       ".erl | xargs sed -n '/-export(/,/)./p' | sed -e '/%/d' | sed -e 's/ //g' | sed -e 's/\\t//g' | sed -e 's/-export.*(\\\[//g' | sed -e 's/\\\]).//g' | sed 's/\\\,/\\\n/g' | sed '/^$/d'"))
     "\n")))
 
-(defun counsel-erl--find-modules ()
+(defun ivy-erlang-complete--find-modules ()
   "Find functions in MODULE."
   (-remove #'string-empty-p
    (split-string
     (shell-command-to-string
      (concat
       "find "
-      counsel-erl-project-root
+      ivy-erlang-complete-project-root
       " "
-      counsel-erl-erlang-root
+      ivy-erlang-complete-erlang-root
       " -iname '*.erl' | xargs basename -a | sed -e 's/\\.erl//g'"))
     "\n")))
 
-(defun counsel-erl--extract-functions (file)
+(defun ivy-erlang-complete--extract-functions (file)
   "Extract all functions from FILE."
   (-remove #'string-empty-p
    (split-string
@@ -63,7 +63,7 @@
       " | sed -e '/%/d' | sed -e '/^\\\-/d' | sed -e '/^[[:space:]]/d' | sed '/^$/d' | sed -e 's/).*/)/g'"))
     "\n")))
 
-(defun counsel-erl--set-arity (erl-function)
+(defun ivy-erlang-complete--set-arity (erl-function)
   "Set arity to ERL-FUNCTION instead of arglist."
   (let ((arity
          (string-trim
@@ -75,27 +75,27 @@
         (string-match "[^(]+" erl-function)
         (concat (substring erl-function (match-beginning 0) (match-end 0)) "/" arity))))
 
-(defun counsel-erl--find-local-functions ()
+(defun ivy-erlang-complete--find-local-functions ()
   "Find all local functions."
-  (mapcar #'counsel-erl--set-arity
-          (counsel-erl--extract-functions (buffer-file-name))))
+  (mapcar #'ivy-erlang-complete--set-arity
+          (ivy-erlang-complete--extract-functions (buffer-file-name))))
 
-(defun counsel-erl-at-point ()
+(defun ivy-erlang-complete-at-point ()
   "Return the erlang thing at point, or nil if none is found."
   (when (thing-at-point-looking-at "[A-Za-z0-9_#?:]+")
     (match-string-no-properties 0)))
 
 ;;;###autoload
-(defun counsel-erl-set-project-root ()
+(defun ivy-erlang-complete-set-project-root ()
   "Set root for current project."
   (interactive)
   (let
       ((dir
         (expand-file-name (read-directory-name
                            "Select project directory:" default-directory))))
-    (setq counsel-erl-project-root dir)))
+    (setq ivy-erlang-complete-project-root dir)))
 
-(defun counsel-erl--insert-candidate (candidate)
+(defun ivy-erlang-complete--insert-candidate (candidate)
   "Insert CANDIDATE at point."
   (if (string-match "\\([^/]+\\)/\\([0-9]+\\)" candidate)
       (let ((arity (string-to-number
@@ -110,31 +110,31 @@
     (ivy-completion-in-region-action (concat candidate ":"))))
 
 ;;;###autoload
-(defun counsel-erl ()
+(defun ivy-erlang-complete ()
   "Erlang completion at point."
   (interactive)
-  (let ((thing (counsel-erl-at-point)))
+  (let ((thing (ivy-erlang-complete-at-point)))
    (if (and thing (string-match "\\([^\:]+\\)\:\\([^\:]*\\)" thing))
        (let ((erl-prefix (substring thing (match-beginning 1) (match-end 1))))
          (progn
-           (setq counsel-erl-candidates
-                 (counsel-erl--find-functions
+           (setq ivy-erlang-complete-candidates
+                 (ivy-erlang-complete--find-functions
                   erl-prefix))
-           (setq counsel-erl-predicate
+           (setq ivy-erlang-complete-predicate
                  (string-remove-prefix (concat erl-prefix ":") thing))))
      (progn
-       (setq counsel-erl-candidates (append (counsel-erl--find-local-functions)
-                                            (counsel-erl--find-modules)
-                                            (counsel-erl--find-functions "erlang")))
-       (setq counsel-erl-predicate thing))))
-  (when (looking-back counsel-erl-predicate (line-beginning-position))
+       (setq ivy-erlang-complete-candidates (append (ivy-erlang-complete--find-local-functions)
+                                            (ivy-erlang-complete--find-modules)
+                                            (ivy-erlang-complete--find-functions "erlang")))
+       (setq ivy-erlang-complete-predicate thing))))
+  (when (looking-back ivy-erlang-complete-predicate (line-beginning-position))
     (setq ivy-completion-beg (match-beginning 0))
     (setq ivy-completion-end (match-end 0)))
-  (ivy-read "Counsel-erl cand:" counsel-erl-candidates
-            :initial-input counsel-erl-predicate
-            :action #'counsel-erl--insert-candidate))
+  (ivy-read "Counsel-erl cand:" ivy-erlang-complete-candidates
+            :initial-input ivy-erlang-complete-predicate
+            :action #'ivy-erlang-complete--insert-candidate))
 
 
 
-(provide 'counsel-erl)
-;;; counsel-erl.el ends here
+(provide 'ivy-erlang-complete)
+;;; ivy-erlang-complete.el ends here
