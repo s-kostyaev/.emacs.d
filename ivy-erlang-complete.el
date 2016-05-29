5d3e53c142a8adc2563e5bd547b8ff57e5eeab5c
Sergey Kostyaev <s.kostyaev@office.ngs.ru>
2016-05-29 16:39:47 +0600

    add license

--- ivy-erlang-complete.el
+++ ivy-erlang-complete.el
@@ -1,12 +1,28 @@
 ;;; ivy-erlang-complete --- Erlang completion at point using ivy.
 
-;;; Commentary:
-;; Deps:
-;;  - coreutils
-;;  - findutils
-;;  - sed
-;;  - grep
 
+;; Copyright (C) 2016 Sergey Kostyaev
+
+;; Author: Sergey Kostyaev <feo.me@ya.ru>
+;; Version: 1.0.0
+;; Keywords: erlang ivy completion
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; `ivy-erlang-complete' is erlang completion package with `ivy' as frontend.
 
 ;;; Code:
 (require 'ivy)
