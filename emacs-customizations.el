d42b685e5727e3d990aae6aa1c0433cfd75e6765
Sergey Kostyaev <sergey.kostyaev@eltex.loc>
2017-04-19 11:53:44 +0700

    add arch-packer

--- emacs-customizations.el
+++ emacs-customizations.el
@@ -15,6 +15,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(arch-packer-default-command "pacaur")
  '(custom-safe-themes
    (quote
     ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
@@ -25,7 +26,7 @@
      ("melpa" . "http://melpa.org/packages/"))))
  '(package-selected-packages
    (quote
-    (org-brain spaceline counsel-dash ivy-historian ibuffer-vc rainbow-mode mu4e-alert pass password-store paradox use-package use-package-chords async zenburn-theme xref-js2 xah-lookup which-key wgrep web-mode web-beautify viking-mode timp tagedit sublime-themes speed-type spacemacs-theme solarized-theme smooth-scrolling smooth-scroll smex smartparens smart-mode-line-powerline-theme slime-company rtags rjsx-mode restclient react-snippets quelpa paredit-menu pandoc-mode pacmacs package-lint noflet nlinum monokai-theme markdown-mode lua-mode link-hint key-chord json-rpc js3-mode js2-refactor jquery-doc ivy-rich ivy-hydra ido-vertical-mode header2 go-impl go-eldoc go-autocomplete geiser fuzzy fsm flymd flycheck-gometalinter flycheck-dialyzer flx fill-column-indicator esup eopengrok ensime embrace edit-indirect darkokai-theme counsel-projectile composable company-tern company-statistics company-quickhelp company-irony-c-headers company-irony company-go company-erlang company-c-headers company-anaconda column-marker column-enforce-mode color-theme-solarized cmake-ide cmake-font-lock cask-mode camcorder auto-complete-clang ace-window ace-mc ace-link ac-js2 ac-emmet ac-cider)))
+    (arch-packer org-brain spaceline counsel-dash ivy-historian ibuffer-vc rainbow-mode mu4e-alert pass password-store paradox use-package use-package-chords async zenburn-theme xref-js2 xah-lookup which-key wgrep web-mode web-beautify viking-mode timp tagedit sublime-themes speed-type spacemacs-theme solarized-theme smooth-scrolling smooth-scroll smex smartparens smart-mode-line-powerline-theme slime-company rtags rjsx-mode restclient react-snippets quelpa paredit-menu pandoc-mode pacmacs package-lint noflet nlinum monokai-theme markdown-mode lua-mode link-hint key-chord json-rpc js3-mode js2-refactor jquery-doc ivy-rich ivy-hydra ido-vertical-mode header2 go-impl go-eldoc go-autocomplete geiser fuzzy fsm flymd flycheck-gometalinter flycheck-dialyzer flx fill-column-indicator esup eopengrok ensime embrace edit-indirect darkokai-theme counsel-projectile composable company-tern company-statistics company-quickhelp company-irony-c-headers company-irony company-go company-erlang company-c-headers company-anaconda column-marker column-enforce-mode color-theme-solarized cmake-ide cmake-font-lock cask-mode camcorder auto-complete-clang ace-window ace-mc ace-link ac-js2 ac-emmet ac-cider)))
  '(paradox-automatically-star nil)
  '(paradox-github-token (password-store-get "paradox-token"))
  '(send-mail-function (quote smtpmail-send-it))
