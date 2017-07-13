9dffa2e92f7de104679c1468fc34f25c4acea778
Sergey Kostyaev <feo.me@ya.ru>
2017-07-13 19:49:39 +0700

    fix compile warning

--- init.el
+++ init.el
@@ -252,6 +252,7 @@
 
 (declare-function go-goto-imports "ext:go-mode")
 (declare-function lsp-define-stdio-client "ext:lsp-mode")
+(declare-function godoc-at-point "ext:go-mode")
 (defvar company-begin-commands)
 (defvar company-backends)
 (defvar flycheck-gometalinter-deadline)
