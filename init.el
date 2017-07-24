ebeb29a68dd59c98bc92710ae3c21699cdd0fce8
Sergey Kostyaev <feo.me@ya.ru>
2017-07-25 01:34:53 +0700

    add indium, json-snatcher & js2-refactor hydra
    for better js developement

--- init.el
+++ init.el
@@ -528,6 +528,73 @@ the end of the line, then comment current line.  Replaces default behaviour of
             (define-key js2-mode-map (kbd "M-.") nil)
             (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t)))
 
+;; js2-refactor: refactoring options for emacs
+;; https://github.com/magnars/js2-refactor.el
+(use-package js2-refactor
+  :diminish js2-refactor-mode "𝐉𝐫"
+  :bind
+  (:map js2-mode-map
+        ("C-c h r" . js2-refactor-hydra/body))
+  :config (js2r-add-keybindings-with-prefix "C-c C-r")
+
+  (defhydra js2-refactor-hydra (:color blue :hint nil)
+    "
+^Functions^                    ^Variables^               ^Buffer^                      ^sexp^               ^Debugging^
+------------------------------------------------------------------------------------------------------------------------------
+[_lp_] Localize Parameter      [_ev_] Extract variable   [_wi_] Wrap buffer in IIFE    [_k_]  js2 kill      [_lt_] log this
+[_ef_] Extract function        [_iv_] Inline variable    [_ig_] Inject global in IIFE  [_ss_] split string  [_dt_] debug this
+[_ip_] Introduce parameter     [_rv_] Rename variable    [_ee_] Expand node at point   [_sl_] forward slurp
+[_em_] Extract method          [_vt_] Var to this        [_cc_] Contract node at point [_ba_] forward barf
+[_ao_] Arguments to object     [_sv_] Split var decl.    [_uw_] unwrap
+[_tf_] Toggle fun exp and decl [_ag_] Add var to globals
+[_ta_] Toggle fun expr and =>  [_ti_] Ternary to if
+[_q_]  quit"
+      ("ee" js2r-expand-node-at-point)
+      ("cc" js2r-contract-node-at-point)
+      ("ef" js2r-extract-function)
+      ("em" js2r-extract-method)
+      ("tf" js2r-toggle-function-expression-and-declaration)
+      ("ta" js2r-toggle-arrow-function-and-expression)
+      ("ip" js2r-introduce-parameter)
+      ("lp" js2r-localize-parameter)
+      ("wi" js2r-wrap-buffer-in-iife)
+      ("ig" js2r-inject-global-in-iife)
+      ("ag" js2r-add-to-globals-annotation)
+      ("ev" js2r-extract-var)
+      ("iv" js2r-inline-var)
+      ("rv" js2r-rename-var)
+      ("vt" js2r-var-to-this)
+      ("ao" js2r-arguments-to-object)
+      ("ti" js2r-ternary-to-if)
+      ("sv" js2r-split-var-declaration)
+      ("ss" js2r-split-string)
+      ("uw" js2r-unwrap)
+      ("lt" js2r-log-this)
+      ("dt" js2r-debug-this)
+      ("sl" js2r-forward-slurp)
+      ("ba" js2r-forward-barf)
+      ("k" js2r-kill)
+      ("q" nil)
+      ))
+
+(add-hook 'js2-mode-hook 'js2-refactor-mode)
+
+
+;; json-snatcher: get the path of any JSON element easily
+;; https://github.com/Sterlingg/json-snatcher
+(use-package json-snatcher
+  :config
+  (defun js-mode-bindings ()
+    "Sets a hotkey for using the json-snatcher plugin"
+    (when (string-match  "\\.json$" (buffer-name))
+      (local-set-key (kbd "C-c C-g") 'jsons-print-path)))
+  (add-hook 'js2-mode-hook 'js-mode-bindings))
+
+;; indium: javascript awesome development environment
+;; https://github.com/NicolasPetton/indium
+(use-package indium
+  :config (add-hook 'js2-mode-hook 'indium-interaction-mode))
+
 ;; tern
 ;; (add-to-list 'company-backends 'company-tern)
 ;; (defvar company-tern-meta-as-single-line)
