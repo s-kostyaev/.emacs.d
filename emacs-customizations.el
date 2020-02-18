4bea1cc6550f52b92f30162a33ea9255c516f793
Sergey Kostyaev <sergey.kostyaev@bitfury.com>
2020-02-18 18:08:53 +0700

    change font

--- emacs-customizations.el
+++ emacs-customizations.el
@@ -9,7 +9,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:height 130 :width normal :family "Go Mono")))))
+ '(default ((t (:height 130 :width normal :family "PT Mono")))))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
