b50bce0e3dd13039f1468b832695243051f6d15a
Sergey Kostyaev <sskostyaev@gmail.com>
2021-04-18 22:25:50 +0700

    don't use find on startup by straight

--- init.el
+++ init.el
@@ -18,6 +18,8 @@
         (setq gnutls-verify-error t))))
 
 (defvar bootstrap-version)
+(defvar straight-check-for-modifications)
+(setq straight-check-for-modifications '(check-on-save find-when-checking))
 (let ((bootstrap-file
        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
       (bootstrap-version 5))
