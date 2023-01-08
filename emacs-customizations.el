352a8469955e053e6a4c3f5d2dfac01a59feeaff
Sergey Kostyaev <s.kostyaev@omp.ru>
2023-01-08 21:38:11 +0700

    fix keycast setup

--- emacs-customizations.el
+++ emacs-customizations.el
@@ -59,7 +59,7 @@
  '(hippie-expand-try-functions-list
    '(try-complete-file-name-partially try-complete-file-name try-expand-all-abbrevs try-expand-list try-expand-line try-expand-dabbrev try-expand-dabbrev-all-buffers try-expand-dabbrev-from-kill))
  '(imenu-max-item-length 230)
- '(keycast-insert-after "        ")
+ '(keycast-insert-after "    ")
  '(leetcode-prefer-language "golang")
  '(leetcode-save-solutions t)
  '(lsp-auto-guess-root t)
