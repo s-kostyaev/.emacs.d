b6cf58e7f58d721d88093927def7c7941044cc60
Sergey Kostyaev <s.kostyaev@omp.ru>
2023-01-08 14:52:58 +0700

    use deferred package loading instead of compilation user config

--- init.el
+++ init.el
@@ -37,17 +37,12 @@ named arguments:
     (unless (package-installed-p pac-name)
       (package-vc-install uri iname rev backend))))
 
+(setq-default use-package-always-defer t)
+
 (setq package-quickstart t)
 
 (setq native-comp-deferred-compilation t)
 (setq native-compile-prune-cache t)
-
-;; (global-set-key (kbd "C-M-r") (lambda () (interactive)
-;;                                 (byte-recompile-file "~/.emacs.d/init.el" t 0)
-;;                                 (native-compile "~/.emacs.d/init.el")
-;;                                 (package-quickstart-refresh)
-;;                                 (byte-recompile-file "~/.emacs.d/package-quickstart.el" t 0)
-;;                                 (native-compile "~/.emacs.d/package-quickstart.el")))
 
 (setq custom-file "~/.emacs.d/emacs-customizations.el")
 
