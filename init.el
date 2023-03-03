4f653fad5da89f7b9a397b69e06cbb87e1bb1004
Sergey Kostyaev <s.kostyaev@omp.ru>
2023-03-03 18:32:38 +0700

    fix trustfiles path

--- init.el
+++ init.el
@@ -7,7 +7,7 @@
 (defvar gnutls-trustfiles)
 (if (eq system-type 'darwin)
     (with-eval-after-load 'gnutls
-      (add-to-list 'gnutls-trustfiles "/usr/local/etc/libressl/cert.pem")))
+      (add-to-list 'gnutls-trustfiles "/opt/homebrew/opt/libressl/.bottle/etc/libressl/cert.pem")))
 
 (eval-and-compile
   (defvar network-security-level)
