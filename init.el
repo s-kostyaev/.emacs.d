9eab19b49e94a5bbffa50ad2acd2726c4b4c6e19
Sergey Kostyaev <s.kostyaev@omp.ru>
2023-10-19 03:48:44 +0700

    use llm with ellama

--- init.el
+++ init.el
@@ -1686,7 +1686,10 @@ _c_lose node   _p_revious fold   toggle _a_ll        e_x_it
   :init
   (my-vc-install :name "ellama" :host "github" :repo "s-kostyaev/ellama")
   (setopt ellama-language "Russian")
-  (setopt ellama-model "zephyr:7b-alpha-q5_K_M"))
+  (setopt ellama-model "zephyr:7b-alpha-q5_K_M")
+  (setopt ellama-provider
+	  (make-llm-ollama
+	   :chat-model "zephyr:7b-alpha-q5_K_M" :embedding-model "zephyr:7b-alpha-q5_K_M")))
 
 (setopt elisp-flymake-byte-compile-load-path load-path)
 
