37933b43f24b077b02d25799cd7bacd24662ea3f
Sergey Kostyaev <kostyaev.sergey2@wb.ru>
2025-01-09 13:36:54 +0700

    Add ultra scroll

--- emacs-customizations.el
+++ emacs-customizations.el
@@ -9,24 +9,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(fringe ((t :background "unspecified-bg")))
- '(header-line ((t :box (:line-width 4 :color "unspecified-bg" :style nil))))
- '(header-line-highlight ((t :box (:color "unspecified-fg"))))
- '(keycast-key ((t)))
- '(line-number ((t :background "unspecified-bg")))
- '(mode-line ((t :box (:line-width 6 :color nil :style nil))))
- '(mode-line-active ((t :box (:line-width 6 :color nil :style nil))))
- '(mode-line-highlight ((t :box (:color "unspecified-fg"))))
- '(mode-line-inactive ((t :box (:line-width 6 :color nil :style nil))))
- '(tab-bar-tab ((t :box (:line-width 4 :color "grey" :style nil))))
- '(tab-bar-tab-inactive ((t :box (:line-width 4 :color "grey" :style nil))))
- '(tab-line-tab ((t)))
- '(tab-line-tab-active ((t)))
- '(tab-line-tab-inactive ((t)))
- '(vertical-border ((t :background "unspecified-bg" :foreground "unspecified-bg")))
- '(window-divider ((t :background "unspecified-bg" :foreground "unspecified-bg")))
- '(window-divider-first-pixel ((t :background "unspecified-bg" :foreground "unspecified-bg")))
- '(window-divider-last-pixel ((t :background "unspecified-bg" :foreground "unspecified-bg"))))
+ )
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -54,9 +37,10 @@
      ("toml" . conf-toml)
      ("html" . web)))
  '(package-selected-packages
-   '(outline-indent haskell-ts-mode ready-player expreg elisa web-mode gotest ob-go pdf-tools casual code-cells eglot cape tabby ellama dape envrc wgrep breadcrumb zenburn-theme xeft white-sand-theme web-beautify vterm-toggle vmd-mode visual-regexp treesit-auto timp tagedit symbol-overlay string-inflection spray speed-type speechd-el spacious-padding spacemacs-theme solarized-theme smart-shift rust-playground rust-mode reverse-im restclient-jq rainbow-mode pyenv-mode pulsar protobuf-mode prism poly-markdown plz pkgbuild-mode pass pandoc-mode package-lint-flymake org-mind-map ocp-indent nova-theme nov noflet multiple-cursors monokai-theme merlin marginalia magit-todos keycast key-chord kaolin-themes jsonian json-snatcher json-rpc ibuffer-vc hungry-delete highlight-indentation hercules go-tag go-snippets go-impl go-gen-test go-fill-struct git-timemachine gif-screencast frimacs flymake-quickdef flymake-proselint flymake-go-staticcheck flx feature-mode fb2-reader expand-region embark-consult elisp-benchmarks ein eglot-fsharp ef-themes edit-server edit-indirect eat dune dumb-jump dtrt-indent dotnet dockerfile-mode docker-compose-mode denote dash-functional dart-mode corfu consult-dash composable comment-tags cl-libify chocolate-theme cask-mode cargo bash-completion auto-yasnippet apparmor-mode ample-theme aggressive-indent ace-link))
+   '(ultra-scroll outline-indent haskell-ts-mode ready-player expreg elisa web-mode gotest ob-go pdf-tools casual code-cells eglot cape tabby ellama dape envrc wgrep breadcrumb zenburn-theme xeft white-sand-theme web-beautify vterm-toggle vmd-mode visual-regexp treesit-auto timp tagedit symbol-overlay string-inflection spray speed-type speechd-el spacious-padding spacemacs-theme solarized-theme smart-shift rust-playground rust-mode reverse-im restclient-jq rainbow-mode pyenv-mode pulsar protobuf-mode prism poly-markdown plz pkgbuild-mode pass pandoc-mode package-lint-flymake org-mind-map ocp-indent nova-theme nov noflet multiple-cursors monokai-theme merlin marginalia magit-todos keycast key-chord kaolin-themes jsonian json-snatcher json-rpc ibuffer-vc hungry-delete highlight-indentation hercules go-tag go-snippets go-impl go-gen-test go-fill-struct git-timemachine gif-screencast frimacs flymake-quickdef flymake-proselint flymake-go-staticcheck flx feature-mode fb2-reader expand-region embark-consult elisp-benchmarks ein eglot-fsharp ef-themes edit-server edit-indirect eat dune dumb-jump dtrt-indent dotnet dockerfile-mode docker-compose-mode denote dash-functional dart-mode corfu consult-dash composable comment-tags cl-libify chocolate-theme cask-mode cargo bash-completion auto-yasnippet apparmor-mode ample-theme aggressive-indent ace-link))
  '(package-vc-selected-packages
-   '((ready-player :vc-backend Git :url "https://www.github.com/xenodium/ready-player")
+   '((ultra-scroll :vc-backend Git :url "https://www.github.com/jdtsmith/ultra-scroll")
+     (ready-player :vc-backend Git :url "https://www.github.com/xenodium/ready-player")
      (elisa :vc-backend Git :url "https://github.com/s-kostyaev/elisa")
      (tabby :vc-backend Git :url "https://www.github.com/alan-w-255/tabby.el")))
  '(reverse-im-input-methods '("russian-computer"))
--- init.el
+++ init.el
@@ -1677,6 +1677,15 @@ Select it interactively otherwise."
 	   )
   (pixel-scroll-precision-mode +1))
 
+(use-package ultra-scroll
+  :preface
+  (my-vc-install :name "ultra-scroll" :host "github" :repo "jdtsmith/ultra-scroll")
+  :init
+  (setq scroll-conservatively 101 ; important!
+        scroll-margin 0)
+  :config
+  (ultra-scroll-mode 1))
+
 (use-package narrow
   :bind (("C-x n n" . my-narrow-dwim))
   :init
