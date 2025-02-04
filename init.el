4e4bb76561bb07616fff45eb456cb7f3bcdf6326
Sergey Kostyaev <kostyaev.sergey2@wb.ru>
2025-02-05 00:44:46 +0700

    Add default chat temperature setting
    Added a new parameter `:default-chat-temperature` with a value of
    `0.15` to the `make-llm-ollama` function call. This parameter is used
    to set the default temperature for chat interactions.

--- init.el
+++ init.el
@@ -1731,6 +1731,7 @@ Select it interactively otherwise."
 	  (make-llm-ollama
 	   :chat-model "mistral-small:24b"
 	   :embedding-model "nomic-embed-text"
+	   :default-chat-temperature 0.15
 	   :default-chat-non-standard-params
 	   '(("num_ctx" . 32768))))
   ;; (setopt ellama-provider
