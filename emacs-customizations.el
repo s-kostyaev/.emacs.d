35603599d304b28fae456b76458a5178f3ed24ec
Sergey Kostyaev <kostyaev.sergey2@wb.ru>
2025-02-24 19:48:44 +0700

    Clean up and format Emacs customizations
    Reformatted `org-src-lang-modes` and `package-selected-packages` for better
    readability. Adjusted the indentation and spacing to enhance code clarity. Also,
    corrected minor issues in `safe-local-variable-values`.
    
    Changes after loading emacs 30.1

--- emacs-customizations.el
+++ emacs-customizations.el
@@ -16,45 +16,52 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(org-src-lang-modes
-   '(("jupyter-python" . python)
-     ("ipython" . python)
-     ("html-chrome" . html)
-     ("C" . c)
-     ("C++" . c++)
-     ("asymptote" . asy)
-     ("bash" . sh)
-     ("beamer" . latex)
-     ("calc" . fundamental)
-     ("cpp" . c++)
-     ("ditaa" . artist)
-     ("desktop" . conf-desktop)
-     ("dot" . fundamental)
-     ("elisp" . emacs-lisp)
-     ("ocaml" . tuareg)
-     ("screen" . shell-script)
-     ("shell" . sh)
-     ("sqlite" . sql)
-     ("toml" . conf-toml)
-     ("html" . web)))
+   '(("jupyter-python" . python) ("ipython" . python) ("html-chrome" . html)
+     ("C" . c) ("C++" . c++) ("asymptote" . asy) ("bash" . sh)
+     ("beamer" . latex) ("calc" . fundamental) ("cpp" . c++) ("ditaa" . artist)
+     ("desktop" . conf-desktop) ("dot" . fundamental) ("elisp" . emacs-lisp)
+     ("ocaml" . tuareg) ("screen" . shell-script) ("shell" . sh)
+     ("sqlite" . sql) ("toml" . conf-toml) ("html" . web)))
  '(package-selected-packages
-   '(diminish treesit-fold csv-mode posframe aider erc faceup flymake idlwave org project soap-client tramp use-package verilog-mode xref ultra-scroll outline-indent haskell-ts-mode expreg elisa web-mode gotest ob-go pdf-tools casual code-cells eglot cape tabby ellama dape envrc wgrep breadcrumb zenburn-theme xeft white-sand-theme web-beautify vterm-toggle vmd-mode visual-regexp treesit-auto timp tagedit symbol-overlay string-inflection spray speed-type speechd-el spacious-padding spacemacs-theme solarized-theme smart-shift rust-playground rust-mode reverse-im restclient-jq rainbow-mode pyenv-mode pulsar protobuf-mode prism poly-markdown plz pkgbuild-mode pass pandoc-mode package-lint-flymake org-mind-map ocp-indent nova-theme nov noflet multiple-cursors monokai-theme merlin marginalia magit-todos keycast key-chord jsonian json-snatcher json-rpc ibuffer-vc hungry-delete highlight-indentation hercules go-tag go-snippets go-impl go-gen-test go-fill-struct git-timemachine gif-screencast frimacs flymake-quickdef flymake-proselint flymake-go-staticcheck flx feature-mode fb2-reader expand-region embark-consult elisp-benchmarks ein eglot-fsharp ef-themes edit-server edit-indirect eat dune dumb-jump dtrt-indent dotnet dockerfile-mode docker-compose-mode denote dash-functional dart-mode corfu consult-dash composable comment-tags cl-libify chocolate-theme cask-mode cargo bash-completion auto-yasnippet apparmor-mode ample-theme aggressive-indent ace-link))
+   '(ace-link aggressive-indent aider ample-theme apparmor-mode auto-yasnippet
+	      bash-completion breadcrumb cape cargo cask-mode casual
+	      chocolate-theme cl-libify code-cells comment-tags composable
+	      consult-dash corfu csv-mode dape dart-mode dash-functional denote
+	      diminish docker-compose-mode dockerfile-mode dotnet dtrt-indent
+	      dumb-jump dune eat edit-indirect edit-server editorconfig
+	      ef-themes eglot eglot-fsharp ein elisa elisp-benchmarks ellama
+	      embark-consult envrc erc expand-region expreg faceup fb2-reader
+	      feature-mode flx flymake flymake-go-staticcheck flymake-proselint
+	      flymake-quickdef frimacs gif-screencast git-timemachine
+	      go-fill-struct go-gen-test go-impl go-snippets go-tag gotest
+	      haskell-ts-mode hercules highlight-indentation hungry-delete
+	      ibuffer-vc idlwave json-rpc json-snatcher jsonian key-chord
+	      keycast magit-todos marginalia merlin monokai-theme
+	      multiple-cursors noflet nov nova-theme ob-go ocp-indent org
+	      org-mind-map outline-indent package-lint-flymake pandoc-mode pass
+	      pdf-tools pkgbuild-mode plz poly-markdown posframe prism project
+	      protobuf-mode pulsar pyenv-mode rainbow-mode restclient-jq
+	      reverse-im rust-mode rust-playground smart-shift soap-client
+	      solarized-theme spacemacs-theme spacious-padding speechd-el
+	      speed-type spray string-inflection symbol-overlay tabby tagedit
+	      timp tramp transient treesit-auto treesit-fold ultra-scroll
+	      use-package verilog-mode visual-regexp vmd-mode vterm-toggle
+	      web-beautify web-mode wgrep which-key white-sand-theme
+	      window-tool-bar xeft xref zenburn-theme))
  '(package-vc-selected-packages
    '((aider :vc-backend Git :url "https://www.github.com/tninja/aider.el")
-     (ultra-scroll :vc-backend Git :url "https://www.github.com/jdtsmith/ultra-scroll")
-     (ready-player :vc-backend Git :url "https://www.github.com/xenodium/ready-player")
+     (ultra-scroll :vc-backend Git :url
+		   "https://www.github.com/jdtsmith/ultra-scroll")
+     (ready-player :vc-backend Git :url
+		   "https://www.github.com/xenodium/ready-player")
      (elisa :vc-backend Git :url "https://github.com/s-kostyaev/elisa")
      (tabby :vc-backend Git :url "https://www.github.com/alan-w-255/tabby.el")))
  '(reverse-im-input-methods '("russian-computer"))
  '(safe-local-variable-values
-   '((eval and buffer-file-name
-	   (not
-	    (eq major-mode 'package-recipe-mode))
-	   (or
-	    (require 'package-recipe-mode nil t)
-	    (let
-		((load-path
-		  (cons "../package-build" load-path)))
-	      (require 'package-recipe-mode nil t)))
+   '((eval and buffer-file-name (not (eq major-mode 'package-recipe-mode))
+	   (or (require 'package-recipe-mode nil t)
+	       (let ((load-path (cons "../package-build" load-path)))
+		 (require 'package-recipe-mode nil t)))
 	   (package-recipe-mode))
      (conda-project-env-path . "pineapple"))))
 
