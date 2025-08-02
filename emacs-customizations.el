a4fe55adc11790bddc638e5c88164439d9c18d1c
Sergey Kostyaev <kostyaev.sergey2@wb.ru>
2025-08-02 08:09:02 +0200

    Add difftastic to package list
    Added difftastic package to the list of installed packages in
    emacs-customizations.el. This package provides a tool for visualizing
    differences in code, enhancing the development workflow with better diff
    visualization capabilities.

--- emacs-customizations.el
+++ emacs-customizations.el
@@ -81,27 +81,27 @@
 	      bash-completion breadcrumb cape cargo cask-mode casual
 	      chocolate-theme cl-libify code-cells comment-tags composable
 	      consult-dash corfu csv-mode dape dart-mode dash-functional denote
-	      diminish docker-compose-mode dockerfile-mode dotnet dtrt-indent
-	      dumb-jump dune eat edit-indirect edit-server editorconfig
-	      ef-themes eglot eglot-fsharp ein elisa elisp-benchmarks ellama
-	      embark-consult envrc erc expand-region expreg faceup fb2-reader
-	      feature-mode flx flymake flymake-go-staticcheck flymake-proselint
-	      flymake-quickdef frimacs gif-screencast git-timemachine
-	      go-fill-struct go-gen-test go-impl go-snippets go-tag gotest
-	      haskell-ts-mode hercules highlight-indentation hungry-delete
-	      ibuffer-vc idlwave json-rpc json-snatcher jsonian key-chord
-	      keycast magit-todos marginalia merlin monokai-theme
-	      multiple-cursors noflet nov nova-theme ob-go ocp-indent org
-	      org-mind-map outline-indent package-lint-flymake pandoc-mode pass
-	      pdf-tools pkgbuild-mode plz poly-markdown posframe prism project
-	      protobuf-mode pulsar pyenv-mode python rainbow-mode restclient-jq
-	      reverse-im rust-mode rust-playground smart-shift soap-client
-	      solarized-theme spacemacs-theme spacious-padding speechd-el
-	      speed-type spray string-inflection symbol-overlay tabby tagedit
-	      timp track-changes tramp transient treesit-auto treesit-fold
-	      ultra-scroll use-package verilog-mode visual-regexp vmd-mode
-	      vterm-toggle web-beautify web-mode wgrep which-key
-	      white-sand-theme window-tool-bar xeft xref zenburn-theme))
+	      difftastic diminish docker-compose-mode dockerfile-mode dotnet
+	      dtrt-indent dumb-jump dune eat edit-indirect edit-server
+	      editorconfig ef-themes eglot eglot-fsharp ein elisa
+	      elisp-benchmarks ellama embark-consult envrc erc expand-region
+	      expreg faceup fb2-reader feature-mode flx flymake
+	      flymake-go-staticcheck flymake-proselint flymake-quickdef frimacs
+	      gif-screencast git-timemachine go-fill-struct go-gen-test go-impl
+	      go-snippets go-tag gotest haskell-ts-mode hercules
+	      highlight-indentation hungry-delete ibuffer-vc idlwave json-rpc
+	      json-snatcher jsonian key-chord keycast magit-todos marginalia
+	      merlin monokai-theme multiple-cursors noflet nov nova-theme ob-go
+	      ocp-indent org org-mind-map outline-indent package-lint-flymake
+	      pandoc-mode pass pdf-tools pkgbuild-mode plz poly-markdown
+	      posframe prism project protobuf-mode pulsar pyenv-mode python
+	      rainbow-mode restclient-jq reverse-im rust-mode rust-playground
+	      smart-shift soap-client solarized-theme spacemacs-theme
+	      spacious-padding speechd-el speed-type spray string-inflection
+	      symbol-overlay tabby tagedit timp track-changes tramp transient
+	      treesit-auto treesit-fold ultra-scroll use-package verilog-mode
+	      visual-regexp vmd-mode vterm-toggle web-beautify web-mode wgrep
+	      which-key white-sand-theme window-tool-bar xeft xref zenburn-theme))
  '(package-vc-selected-packages
    '((aider :vc-backend Git :url "https://www.github.com/tninja/aider.el")
      (ultra-scroll :vc-backend Git :url
