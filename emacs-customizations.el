ec93b5e832a4e49bf1fed3f43cb7d329c7f22c69
Sergey Kostyaev <kostyaev.sergey2@wb.ru>
2026-07-14 10:53:46 +0200

    Install ghostel instead of vterm

--- emacs-customizations.el
+++ emacs-customizations.el
@@ -101,21 +101,21 @@
 	      eglot-fsharp ein eldoc elisa elisp-benchmarks ellama
 	      embark-consult envrc erc expand-region expreg faceup fb2-reader
 	      feature-mode flx flymake flymake-go-staticcheck flymake-proselint
-	      flymake-quickdef frimacs futur gif-screencast git-timemachine
-	      go-fill-struct go-gen-test go-impl go-snippets go-tag gotest
-	      haskell-ts-mode hercules highlight-indentation hungry-delete
-	      ibuffer-vc idlwave json-rpc json-snatcher jsonian jsonrpc
-	      key-chord keycast magit-todos marginalia mcp merlin monokai-theme
-	      multiple-cursors neocaml noflet nov nova-theme ob-go ocp-indent
-	      org org-mind-map outline-indent package-lint-flymake pandoc-mode
-	      pass pdf-tools peg pkgbuild-mode plz poly-markdown posframe prism
-	      project protobuf-mode pulsar pyenv-mode python rainbow-mode
-	      restclient-jq reverse-im rust-mode rust-playground smart-shift
-	      soap-client solarized-theme spacemacs-theme spacious-padding
-	      speechd-el speed-type spray string-inflection symbol-overlay tabby
-	      tagedit timp track-changes tramp transient treesit-auto
-	      treesit-fold ultra-scroll use-package verilog-mode visual-regexp
-	      vmd-mode vterm-toggle web-beautify web-mode wgrep which-key
+	      flymake-quickdef frimacs futur ghostel gif-screencast
+	      git-timemachine go-fill-struct go-gen-test go-impl go-snippets
+	      go-tag gotest haskell-ts-mode hercules highlight-indentation
+	      hungry-delete ibuffer-vc idlwave json-rpc json-snatcher jsonian
+	      jsonrpc key-chord keycast magit-todos marginalia mcp merlin
+	      monokai-theme multiple-cursors neocaml noflet nov nova-theme ob-go
+	      ocp-indent org org-mind-map outline-indent package-lint-flymake
+	      pandoc-mode pass pdf-tools peg pkgbuild-mode plz poly-markdown
+	      posframe prism project protobuf-mode pulsar pyenv-mode python
+	      rainbow-mode restclient-jq reverse-im rust-mode rust-playground
+	      smart-shift soap-client solarized-theme spacemacs-theme
+	      spacious-padding speechd-el speed-type spray string-inflection
+	      symbol-overlay tabby tagedit timp track-changes tramp transient
+	      treesit-auto treesit-fold ultra-scroll use-package verilog-mode
+	      visual-regexp vmd-mode web-beautify web-mode wgrep which-key
 	      white-sand-theme window-tool-bar xeft xref yaml zenburn-theme))
  '(package-vc-selected-packages
    '((aider :vc-backend Git :url "https://www.github.com/tninja/aider.el")
